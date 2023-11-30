ed7a796a6982056c3cda1022b4952a3677991bd4
NajmaSulaiman <najmasulaiman1999@gmail.com>
2023-11-30 12:24:07 +0400

    sqrt

---
+++ sqr.py
@@ -0,0 +1,22 @@
+from math import *
+def sqrt_find(x):
+    l=1
+    h=x
+    
+    if(x==1 or x==0):
+        return x
+   
+    while l<=h:
+        mid=(l+h)//2
+        if (mid*mid==x):
+            return mid
+        elif (mid*mid<x):
+            l=mid+1
+            
+        else:
+            h=mid-1
+    return -1
+
+x=int(input("enter a number: "))
+reslt=print("Your target is in index: ",sqrt_find(x))
+
